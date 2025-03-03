ba58495080550b02edef7b9ff95f987c4145724c
SimPham02 <147654031+SimPham02@users.noreply.github.com>
2025-03-03 14:45:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js-selfbot-v13');
+require('dotenv').config();
 const client = new Discord.Client({
   readyStatus: false,
   checkUpdate: false
@@ -15,57 +16,59 @@ function formatTime() {
     return new Intl.DateTimeFormat('en-US', options).format(date); 
   }
 
-const largeImageMessageUrl = 'https://discord.com/channels/1132649645655461888/1177698786278117487/1233956279689416744';
-const smallImageMessageUrl = 'https://discord.com/channels/1132649645655461888/1132649646125219912/1225292740464480327';
+const largeImageMessageUrl = process.env.LARGE_IMAGE_URL;
+const smallImageMessageUrl = process.env.SMALL_IMAGE_URL;
   
+let currentLargeImageURL = '';
+let currentSmallImageURL = '';
+
 client.on('ready', async () => {
   console.clear();
-  console.log(`${client.user.tag} - login successful`);
-  
+  console.log(`${client.user.tag} - login success`);
+
   const r = new Discord.RichPresence()
-    .setApplicationId('1102290365937090721')
-    .setType('STREAMING')
-    .setURL('') //link tw or ytb
-    .setState('')
-    .setName('')
+    .setApplicationId(process.env.APPLICATION_ID)
+    .setType(process.env.TYPE)
+    .setState(process.env.STATE)
+    .setName(process.env.NAME)
     .setDetails(`sleep [${formatTime()}]`)
     .setStartTimestamp(Date.now())
-    .setAssetsLargeText('')
-    .setAssetsSmallText('')
-    .addButton("", "")
-  
-  async function updateImages() {
+    .setAssetsLargeText(process.env.SET_ASSETS_LARGE_TEXT)
+    .setAssetsSmallText(process.env.SET_ASSETS_SMALL_TEXT)
+    .addButton(process.env.BUTTON_1_LABEL, process.env.BUTTON_1_URL)
+    .addButton(process.env.BUTTON_2_LABEL, process.env.BUTTON_2_URL);
+
+  async function checkAndUpdateImages() {
     try {
       const [largeImageGuildId, largeImageChannelId, largeImageMessageId] = largeImageMessageUrl.split('/').slice(-3);
       const largeImageChannel = await client.channels.fetch(largeImageChannelId);
       const largeImageMessage = await largeImageChannel.messages.fetch(largeImageMessageId);
       if (largeImageMessage.attachments.size > 0) {
         const largeImageAttachment = largeImageMessage.attachments.first();
-        const largeImageURL = largeImageAttachment.url;
-        r.setAssetsLargeImage(largeImageURL);
-      } else {
-        console.log("No attachments found in the specified message for the large image.");
+        const newLargeImageURL = largeImageAttachment.url;
+        r.setAssetsLargeImage(newLargeImageURL);
+        currentLargeImageURL = newLargeImageURL;
       }
+
       const [smallImageGuildId, smallImageChannelId, smallImageMessageId] = smallImageMessageUrl.split('/').slice(-3);
       const smallImageChannel = await client.channels.fetch(smallImageChannelId);
       const smallImageMessage = await smallImageChannel.messages.fetch(smallImageMessageId);
-        
       if (smallImageMessage.attachments.size > 0) {
         const smallImageAttachment = smallImageMessage.attachments.first();
-        const smallImageURL = smallImageAttachment.url;
-        r.setAssetsSmallImage(smallImageURL);
-      } else {
-        console.log("No attachments found in the specified message for the small image.");
+        const newSmallImageURL = smallImageAttachment.url;
+        r.setAssetsSmallImage(newSmallImageURL);
+        currentSmallImageURL = newSmallImageURL;
       }
+
       client.user.setActivity(r);
+      console.log("Đã cập nhật Rich Presence img.");
     } catch (error) {
-      console.error("Error fetching the messages:", error);
+      console.error("Lỗi khi lấy ảnh:", error);
     }
   }
-  await updateImages();
-    // Update images every 5 minutes
-  setInterval(updateImages, 300000);
-  
+
+  await checkAndUpdateImages();
+
   client.user.setPresence({ status: "dnd" });
   
   let prevTime = null;
@@ -80,4 +83,20 @@ client.on('ready', async () => {
   }, 1000);
 });
 
-client.login("");
+async function reconnect() {
+  try {
+    console.log('Đang disconnect...');
+    await client.destroy(); 
+    console.log('Đã disconnect thành công');
+    await new Promise(resolve => setTimeout(resolve, 15 * 1000));
+    console.log('Đang kết nối lại...');
+    await client.login(process.env.DISCORD_TOKEN);
+    console.log('Đã kết nối lại thành công');
+  } catch (error) {
+    console.error('Lỗi khi reconnect:', error);
+  }
+}
+
+setInterval(reconnect, 6 * 60 * 60 * 1000);
+
+client.login(process.env.DISCORD_TOKEN);
