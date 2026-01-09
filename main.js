aa7ba3edc48764266463d7616dfc708ecf2c7402
SimPham02 <phamdinhtungsim@gmail.com>
2026-01-09 09:42:22 +0700

    fix

---
+++ main.js
@@ -0,0 +1,34 @@
+const { fork } = require("child_process");
+
+// Fork index.js chạy suốt
+fork("./index.js");
+
+let accProcess = null;
+
+// Hàm bật acc.js
+function startAcc() {
+  console.log("▶️ Bật acc.js");
+  accProcess = fork("./acc.js");
+
+  // Sau 6 tiếng thì tắt
+  setTimeout(() => {
+    stopAcc();
+  }, 6 * 60 * 60 * 1000); // 6 tiếng
+}
+
+// Hàm tắt acc.js
+function stopAcc() {
+  if (accProcess) {
+    console.log("⛔ Tắt acc.js");
+    accProcess.kill();
+    accProcess = null;
+
+    // Sau 10s bật lại
+    setTimeout(() => {
+      startAcc();
+    }, 10 * 1000);
+  }
+}
+
+// Bắt đầu lần đầu
+startAcc();
--- start.bat
+++
@@ -1,4 +0,0 @@
-:start
-node index.js
-timeout /t 15
-goto start 
