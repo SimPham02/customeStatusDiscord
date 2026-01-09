d44100d03aaa25f16b76f64b5390d1594815c574
SimPham02 <phamdinhtungsim@gmail.com>
2026-01-09 10:16:22 +0700

    Update main.js

--- main.js
+++ main.js
@@ -2,30 +2,21 @@ const { fork } = require("child_process");
 
 let accProcess = null;
 
-// Hàm bật acc.js
 function startAcc() {
-  console.log("▶️ Bật acc.js");
   accProcess = fork("./acc.js");
-
-  // Sau 6 tiếng thì tắt
   setTimeout(() => {
     stopAcc();
-  }, 6 * 60 * 60 * 1000); // 6 tiếng
+  }, 6 * 60 * 60 * 1000);
 }
 
-// Hàm tắt acc.js
 function stopAcc() {
   if (accProcess) {
-    console.log("⛔ Tắt acc.js");
     accProcess.kill();
     accProcess = null;
-
-    // Sau 10s bật lại
     setTimeout(() => {
       startAcc();
     }, 10 * 1000);
   }
 }
 
-// Bắt đầu lần đầu
 startAcc();
